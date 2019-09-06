68d244cfbe41a0d8b15f3d58c34a0484f00ca098
Alex Costa <alex@deanx.com.br>
2019-09-06 13:07:59 +0200

    adapting code to azure interface

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const fetch = require('node-fetch');
 const credentials = require('./lib/credentials.js');
 const anypointmq = require('./lib/anypointmq.js');
 
-credentials.getCredentials()
-              .then((credentials) => anypointmq.putMessage(JSON.parse(credentials), {
-                  "body": "{\"container\": \"xom\",\"fileName\":\"invoice-xxxx-sample.xml\"}"
-})).then(console.log);
+module.exports = async function (context, blob) {
+    const credentials = await credentials.getCredentials();
+    anypointmq.putMessage(JSON.parse(credentials), {
+        "body": "{\"container\": \"xom\",\"fileName\":\"" + context.bindingData.name + "\"}"})
+        .then(context.log)
+        .catch(context.log);
+    
+    //context.log(Object.getOwnPropertyNames(context.bindingData.metadata));
+    //context.log("JavaScript blob trigger function processed blob \n Name:", context.bindingData.name, "\n Blob Size:", blob.length, "Bytes");
+};
